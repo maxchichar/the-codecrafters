10c3759d5758fa1f106947841f8618d1bc0d421a
maxchichar <maxwellchibuezecharles@gmail.com>
2026-03-30 19:04:35 +0100

    code for reverse incomplete

---
+++ string-transformer/module-string-transformer/reverse.go
@@ -0,0 +1,24 @@
+// CodeCrafters — Operation Gopher Protocol
+// Module: String Transformer
+// Author: [Chibueze Maxwell]
+// Squad:  [Goroutines]
+
+package modulestringtransformer
+
+import(
+	strings
+)
+
+func Reversed(text string) string {
+	
+	if strings.HasPrefix(text, "reverse") {
+		strings.TrimPrefix(text, "reverse")
+	}
+
+	for _, v := range v {
+		
+	}
+	for j := 0;len(text) - 1; j-- {
+		
+	}
+}

930d176fb517fdb3e1490d9a35034b444046ea6d
maxchichar <maxwellchibuezecharles@gmail.com>
2026-03-31 17:34:29 +0100

    working on error handling

--- string-transformer/main.go
+++ string-transformer/main.go
@@ -6,15 +6,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strconv"
+	"os"
+	"regexp"
 	"strings"
 	"time"
-	"regexp"
 )
 
-var choice string
-var word string
 
 func ToCapital(text string) string {
 	s := strings.ToLower(text) //converts the text to lowercase
@@ -30,6 +29,7 @@ func ToCapital(text string) string {
 }
 
 func ToLower(text string) string {
+	text = strings.ToLower(text)
 	if strings.HasPrefix(text, "lower") {
 		if len(text) > 0 {
 			text = strings.TrimPrefix(text, "lower") // removes lower from the output
@@ -40,6 +40,7 @@ func ToLower(text string) string {
 }
 
 func ToUpper(text string) string {
+	text = strings.ToLower(text)
 	if strings.HasPrefix(text, "upper") {
 		text = strings.TrimPrefix(text, "upper")
 		text = strings.ToUpper(text)
@@ -48,6 +49,7 @@ func ToUpper(text string) string {
 }
 
 func Title(text string) string {
+	text = strings.ToLower(text)
 	words := strings.Fields(text)
 	smallwords := " a an the and but or for nor on at to by in of up as is it "
 	
@@ -111,78 +113,96 @@ func ReversedText(input string) string {
 	return strings.Join(reversedWords, " ")
 }
 
+func showHelp() {
+	fmt.Println("Sentinel String Transformer Help")
+	fmt.Println("──────────────────────────────────────")
+	fmt.Println()
+
+	helptext := `
+	Usage: 
+		<command> <text>
+	Command:
+		> upper     <text> 
+		> snake     <text> 
+		> lower     <text> 
+		> cap 	    <text> 
+		> reverse   <text> 
+		> title 	<text>
+		> exit
+	Example:
+		> upper sentinel online
+		> SENTINEL ONLINE
+
+		> exit
+		> Shutting down String Transformer...
+
+	`
+	fmt.Print(helptext)
+
+}
+
 func main()  {
 	fmt.Println("SENTINEL STRING TRANSFORMER — ONLINE")
   	fmt.Println("──────────────────────────────────────")
-	  fmt.Println()
+	fmt.Println()
 
 	fmt.Print("S.S.T Loading...")
   	time.Sleep(3 * time.Second)
 	fmt.Println()
 	  
-	for {
-		fmt.Println()
-		fmt.Println("Menu Loading...")
-		time.Sleep(2 * time.Second)
-		fmt.Println()
-		
-		fmt.Println("Sentinel String Transformer Menu")
-		fmt.Println("────────────────────────────────")
-		fmt.Println("⚠️ Select option using numbers ❗")
-		set1 := 1 
-		fmt.Println(set1, "To uppercase")
-		set2 :=	2
-		fmt.Println(set2, "To lowercase")
-		set3 := 3
-		fmt.Println(set3, "Capitalize")
-		set4 := 4
-		fmt.Println(set4, )
-		set5 := 5
-		fmt.Println(set5)
-		set6 := 6
-		fmt.Println(set6)
-		set7 := 7
-		fmt.Println(set7, "Exit")
 
-		fmt.Println()
-		fmt.Print("Enter option: ")
-		fmt.Scanln(&choice)
+	fmt.Println()
+	fmt.Println("Menu Loading...")
+	time.Sleep(2 * time.Second)
+	fmt.Println()
 		
-		option, err := strconv.Atoi(choice)
-		if err == nil {
-			continue
-		}
-		switch choice {
-		case "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z":
-			fmt.Println("Please select the option by number: ")
-			fmt.Scanln(&choice)
-			continue
-		case "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z":
-			fmt.Println("Please select the option by number: ")
-			fmt.Scanln(&choice)
-			continue
-		}
-		
-		if option == 1 {
-			fmt.Println("Welcome to Sentinel Uppercase Transformer")
-			fmt.Println()
+	fmt.Println("Sentinel String Transformer Menu")
+	fmt.Println("────────────────────────────────")
+	fmt.Println("Input 'help' to start")
+	fmt.Println()
+	
+	scanner := bufio.NewScanner(os.Stdin)
 
-			fmt.Println("Instructions Loading...")
-			time.Sleep(1 * time.Second)
-			fmt.Println()
+	start:
+	fmt.Print("> ")
+	scanner.Scan() 
+	input := scanner.Text()
 
-			fmt.Print("User Input instruction: ")
-			fmt.Println("upper <text>")
-			
-			
-			// upper <text>
-		}
-		if option == 7{
-			return
-		}
-		
+	if input == " " {
+		fmt.Println("Input Invalid: Type 'help' for info.")
+		goto start
 	}
 
+	word := strings.Fields(input)
+	
+	
+	switch word[0] {
+	case "help", "Help", "HELP":
+		showHelp()
+		goto start
+	case "upper", "UPPER", "Upper":
+		fmt.Println(ToUpper(input))
+		goto start
+	case "lower", "Lower", "LOWER":
+		fmt.Println(ToLower(input))
+		goto start
+	case "cap", "Cap", "CAP":
+		fmt.Println(ToCapital(input))
+		goto start
+	case "title", "Title", "TITLE":
+		fmt.Println(Title(input))
+		goto start
+	case "snake", "Snake", "SNAKE":
+		fmt.Println(snakeCase(input))
+		goto start
+	case "reverse", "Reverse", "REVERSE":
+		fmt.Println(ReversedText(input))
+		goto start
+	case "exit", "Exit", "EXIT":
+		fmt.Println("Shutting down String Transformer...")
+		time.Sleep(2 * time.Second)
+		fmt.Println()
+		fmt.Println("Good")
+	}
 
-  
 }

aa8c88b6b2fbee81006e951c4e2eefafceae21ce
maxchichar <maxwellchibuezecharles@gmail.com>
2026-03-31 01:05:09 +0100

    Code updated

--- string-transformer/main.go
+++ string-transformer/main.go
@@ -10,28 +10,106 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"regexp"
 )
 
 var choice string
 var word string
 
-func ToUpper(word string) string {
-	return strings.ToUpper(word)
+func ToCapital(text string) string {
+	s := strings.ToLower(text) //converts the text to lowercase
+	strings.Fields(text)
+	// As the name implies prefix: it checks for the prefix before execution
+	if strings.HasPrefix(s, "cap") {
+		s = strings.TrimPrefix(s, "cap")
+		if len(s) > 0 {
+			s = strings.Title(s)
+		}
+	}
+	return s
 }
 
-func ToLower(word string) string {
-	return strings.ToLower(word)
+func ToLower(text string) string {
+	if strings.HasPrefix(text, "lower") {
+		if len(text) > 0 {
+			text = strings.TrimPrefix(text, "lower") // removes lower from the output
+			text = strings.ToLower(text) //converts any text to lower case
+		}
+	}
+	return text
 }
 
-// func Capitalise(word string) string {
-// 	start := len(word)
-// 	if start >= 0 {
-// 		fmt.Errorf("Error Empty, Input a Word: ")
-// 	}
+func ToUpper(text string) string {
+	if strings.HasPrefix(text, "upper") {
+		text = strings.TrimPrefix(text, "upper")
+		text = strings.ToUpper(text)
+	}
+	return text
+}
 
-// 	// start[:1] + start[1:]
+func Title(text string) string {
+	words := strings.Fields(text)
+	smallwords := " a an the and but or for nor on at to by in of up as is it "
 	
-// }
+	
+	for i, w := range words {
+		if strings.Contains(smallwords, " "+w+" ") {
+			words[i] = w
+		}else{
+			words[i] = strings.Title(w)
+		}
+	}
+	
+	if len(words) > 0 && strings.ToLower(words[0]) == "title" {
+		strings.TrimPrefix(words[0], "title")
+		words = words[1:]
+	}
+
+	words[0] = strings.Title(words[0])
+	return strings.Join(words, " ")
+}
+
+var nonAlphanumericRegex = regexp.MustCompile(`[^a-zA-Z0-9 ]+`)
+func snakeCase(text string) string {
+	text = strings.ToLower(text)
+	text = nonAlphanumericRegex.ReplaceAllString(text, "")
+
+	word := strings.Fields(text)
+
+	if len(word) > 0 && strings.ToLower(word[0]) == "snake" {
+		word = word[1:]
+	}
+	
+	return strings.Join(word, "_")
+}
+
+func Reverse(text string) string {
+	text = strings.ToLower(text)
+
+	if strings.HasPrefix(text, "reverse") && strings.ToLower(text) == "reverse" {
+		strings.TrimPrefix(text, "reverse")
+	}
+
+	runes := []rune(text)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
+
+func ReversedText(input string) string {
+	words := strings.Fields(input)
+
+	reversedWords := []string{}
+
+	for _, word := range words {
+		reversedWords = append(reversedWords, Reverse(word))
+	}
+
+	return strings.Join(reversedWords, " ")
+}
 
 func main()  {
 	fmt.Println("SENTINEL STRING TRANSFORMER — ONLINE")

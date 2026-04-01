e4b9642ab7cebdcb7d9d170e1e116354403210fe
Aso07 <agenesunday143@gmail.com>
2026-04-01 14:56:21 +0100

    punct

---
+++ sentinel-reborn/processor/punct.go
@@ -0,0 +1,24 @@
+// Agene Okoh
+
+package processor
+import (
+	"strings"
+)
+func FixPunctuation(s string) string {
+	s = strings.Join(strings.Fields(s), " ")
+	
+		s = strings.ReplaceAll(s, " ,", ",")
+		s = strings.ReplaceAll(s, " .", ".")
+		s = strings.ReplaceAll(s, " !", "!")
+		s = strings.ReplaceAll(s, " ?", "?")
+		s = strings.ReplaceAll(s, " :", ":")
+		s = strings.ReplaceAll(s, " ;", ";")
+		
+	
+	return s
+
+}
+func HasPunctuation(s string) bool {
+	return strings.ContainsAny(s,",.?:!")
+
+}
